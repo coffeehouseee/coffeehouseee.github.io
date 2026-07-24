1f7eb55ee2667c8c1e452295a65853bdbcdb88f0
josytoin <iambeutfl@gmail.com>
2026-07-24 13:40:38 -0500

    added discussions

--- inject_comments.py
+++ inject_comments.py
@@ -86,10 +86,10 @@ GISCUS_SNIPPET = """
 
     var s = document.createElement('script');
     s.src = 'https://giscus.app/client.js';
-    s.setAttribute('data-repo', 'opengeos/myst-blog-template');           // TODO: Update
-    s.setAttribute('data-repo-id', 'R_kgDOSEV_NQ');         // TODO: Update
+    s.setAttribute('data-repo', 'justinlin-dl/justinlin-dl.github.io');           // TODO: Update
+    s.setAttribute('data-repo-id', 'R_kgDOThnMgg');         // TODO: Update
     s.setAttribute('data-category', 'General');              // TODO: Update
-    s.setAttribute('data-category-id', 'DIC_kwDOSEV_Nc4C6-8R');   // TODO: Update
+    s.setAttribute('data-category-id', 'DIC_kwDOThnMgs4DB01I');   // TODO: Update
     s.setAttribute('data-mapping', 'pathname');
     s.setAttribute('data-strict', '0');
     s.setAttribute('data-reactions-enabled', '1');
